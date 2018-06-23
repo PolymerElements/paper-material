be6cd4cb1528ccf7fd1e9c4a8e0fe5fb79e77530
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:39 -0700

    auto-converted by polymer-modulizer

---
+++ paper-material.js
@@ -0,0 +1,75 @@
+/**
+@license
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+/**
+Material design: [Cards](https://www.google.com/design/spec/components/cards.html)
+
+`paper-material` is a container that renders two shadows on top of each other to
+create the effect of a lifted piece of paper.
+
+Example:
+
+    <paper-material elevation="1">
+      ... content ...
+    </paper-material>
+
+@group Paper Elements
+@demo demo/index.html
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import '@polymer/polymer/polymer-legacy.js';
+
+import '@polymer/paper-styles/shadow.js';
+import './paper-material-shared-styles.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
+    <style include="paper-material-shared-styles"></style>
+    <style>
+      :host([animated]) {
+        @apply --shadow-transition;
+      }
+      :host {
+        @apply --paper-material;
+      }
+    </style>
+
+    <slot></slot>
+`,
+
+  is: 'paper-material',
+
+  properties: {
+    /**
+     * The z-depth of this element, from 0-5. Setting to 0 will remove the
+     * shadow, and each increasing number greater than 0 will be "deeper"
+     * than the last.
+     *
+     * @attribute elevation
+     * @type number
+     * @default 1
+     */
+    elevation: {type: Number, reflectToAttribute: true, value: 1},
+
+    /**
+     * Set this to true to animate the shadow when setting a new
+     * `elevation` value.
+     *
+     * @attribute animated
+     * @type boolean
+     * @default false
+     */
+    animated: {type: Boolean, reflectToAttribute: true, value: false}
+  }
+});
